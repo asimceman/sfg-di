8ef4f42de95060436bc6ae5c9415c41aa403ab7f
asimceman <aceman1@etf.unsa.ba>
2021-08-17 22:00:35 +0200

    State after 36

--- src/test/java/guru/springframework/sfgdi/controllers/ContructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/ContructorInjectedControllerTest.java
@@ -1,19 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+class ConstructorInjectedControllerTest {
 
-class ContructorInjectedControllerTest {
-
-    ContructorInjectedController controller;
+    ConstructorInjectedController controller;
 
 
     @BeforeEach
     void setUp() {
-        controller = new ContructorInjectedController(new GreetingServiceImpl());
+        controller = new ConstructorInjectedController(new ConstructorGreetingService());
     }
 
     @Test
